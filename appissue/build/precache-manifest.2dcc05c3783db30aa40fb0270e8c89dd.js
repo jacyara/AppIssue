2db54287490c35e8345b434a74fde51365bcd919
jacyara <jacyarabosse@gmail.com>
2019-09-13 15:32:26 -0300

    react app

---
+++ appissue/build/precache-manifest.2dcc05c3783db30aa40fb0270e8c89dd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71a0fd00c25665e7faaceab047f4c37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3ff2c84f00e8cd48dcc",
+    "url": "/static/js/2.87ec8f77.chunk.js"
+  },
+  {
+    "revision": "6d08149049b9e6bbb60c",
+    "url": "/static/js/main.47b745e5.chunk.js"
+  },
+  {
+    "revision": "ecad87cf8b2e29da152b",
+    "url": "/static/js/runtime~main.f7bf05e6.js"
+  }
+]);
---
+++ appissue/src/views/Jacy.jsx
@@ -0,0 +1,6 @@
+import { Button } from "bold-ui";
+import React from "react";
+
+export default function Jacy() {
+  return <Button>OIII</Button>;
+}
